996d99c06b0a95e7b4213eb4443a41b0e8f68beb
Felicia Davis <davisfeliciam@gmail.com>
2018-10-27 19:52:52 -0700

    Updated Enemy function to ES6 class syntax

--- js/app.js
+++ js/app.js
@@ -1,26 +1,28 @@
 // Enemies our player must avoid
-const Enemy = function () {
-	// Variables applied to each of our instances go here,
-	// we've provided one for you to get started
+class Enemy {
 
-	// The image/sprite for our enemies, this uses
-	// a helper we've provided to easily load images
-	this.sprite = 'images/enemy-bug.png';
-};
+	constructor() {
+		// Variables applied to each of our instances go here,
+		// we've provided one for you to get started
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function (dt) {
-	// You should multiply any movement by the dt parameter
-	// which will ensure the game runs at the same speed for
-	// all computers.
-};
+		// The image/sprite for our enemies, this uses
+		// a helper we've provided to easily load images
+		this.sprite = 'images/enemy-bug.png';
+	}
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function () {
-	ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+	// Update the enemy's position, required method for game
+	// Parameter: dt, a time delta between ticks
+	update(dt) {
+		// You should multiply any movement by the dt parameter
+		// which will ensure the game runs at the same speed for
+		// all computers.
+	}
 
+	// Draw the enemy on the screen, required method for game
+	render() {
+		ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+	}
+}
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
